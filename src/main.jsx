6e190296715372af63631ca57765644755ccf2ab
VinayPrajwal226 <vinayprajwal226@gmail.com>
2025-10-31 16:59:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -69,7 +69,7 @@ async function initializeApp() {
       <AuthProvider>
         <ParallaxProvider>
           <RouterProvider router={router} />
-        </Provider>
+        </ParallaxProvider>
       </AuthProvider>
     </StrictMode>
   );
